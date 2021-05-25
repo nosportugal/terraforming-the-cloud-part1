be07198b6cec8ddad6e408a7c4e721392ea00a9b
Carlos Alexandre <carlos.alexandre@nos.pt>
2021-05-25 09:24:09 +0100

    updated terraform to 0.15.4

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tfversion=0.14.10
+tfversion=0.15.4
 
 wget "https://releases.hashicorp.com/terraform/$tfversion/terraform_${tfversion}_linux_amd64.zip"
 unzip -o terraform_${tfversion}_linux_amd64.zip
